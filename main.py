6cb0e9a72806583fdb1dc344b1430980c49ccfd0
andrei-229 <andrejbelak708@gmail.com>
2022-05-03 20:55:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -281,5 +281,6 @@ def main():
     app.run()
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == 'main':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
